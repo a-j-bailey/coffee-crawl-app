2b37e4837529997e1380e5244f12b225f8728985
Adam Bailey <ajbailey564@gmail.com>
2023-04-30 08:20:05 -0500

    coupon mockup

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -15,7 +15,7 @@ const Profile = ({navigation, route}) => {
   const {user} = useData();
   const {t} = useTranslation();
 //  const navigation = useNavigation();
-  const {assets, colors, sizes} = useTheme();
+  const {assets, colors, gradients, sizes} = useTheme();
 
   const IMAGE_SIZE = (sizes.width - (sizes.padding + sizes.sm) * 2) / 3;
   const IMAGE_VERTICAL_SIZE =
@@ -41,92 +41,89 @@ const Profile = ({navigation, route}) => {
   );
 
   return (
-    <Block safe marginTop={sizes.md}>
-      <Block
-        scroll
-        paddingHorizontal={sizes.s}
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{paddingBottom: sizes.padding}}>
+      <Block safe marginTop={sizes.md}>
+          <Block
+            scroll
+            paddingHorizontal={sizes.s}
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{paddingBottom: sizes.padding}}>
         <Block flex={0}>
           <Image
             background
             resizeMode="cover"
-            padding={sizes.sm}
-            paddingBottom={sizes.l}
             radius={sizes.cardRadius}
             source={{uri: image}}>
-            <Button
-              row
-              flex={0}
-              justify="flex-start"
-              onPress={() => navigation.goBack()}>
-              <Image
-                radius={0}
-                width={10}
-                height={18}
-                color={colors.white}
-                source={assets.arrow}
-                transform={[{rotate: '180deg'}]}
-              />
-              <Text p white marginLeft={sizes.s}>{/*back nav text*/}</Text>
-            </Button>
-            <Block flex={0} align="center">
-              <Image
-                width={64}
-                height={64}
-                marginBottom={sizes.sm}
-                source={{uri: user?.avatar}}
-              />
-              <Text h5 center white>
-                {title}
-              </Text>
-              <Text p center white>
-                {location}
-              </Text>
-              <Block row marginVertical={sizes.m}>
+              <Block color="rgba(0,0,0,0.5)" padding={sizes.padding} paddingBottom={sizes.l}>
                 <Button
-                  white
-                  outlined
-                  shadow={false}
-                  radius={sizes.m}
-                  onPress={() => {alert(`Follow`);}}>
-                  <Block
-                    justify="center"
-                    radius={sizes.m}
-                    paddingHorizontal={sizes.m}
-                    color="rgba(255,255,255,0.2)">
-                    <Text white bold transform="uppercase">
-                      {t('common.follow')}
-                    </Text>
+                  row
+                  flex={0}
+                  justify="flex-start"
+                  onPress={() => navigation.goBack()}>
+                  <Image
+                    radius={0}
+                    width={10}
+                    height={18}
+                    color={colors.white}
+                    source={assets.arrow}
+                    transform={[{rotate: '180deg'}]}
+                  />
+                  <Text p white marginLeft={sizes.s}>{/*back nav text*/}</Text>
+                </Button>
+                <Block flex={0} align="center">
+                  {/*<Image
+                    width={64}
+                    height={64}
+                    marginBottom={sizes.sm}
+                    source={{uri: user?.avatar}}
+                  />*/}
+                  <Text h4 center white>
+                    {title}
+                  </Text>
+                  <Text p center white>
+                    {location}
+                  </Text>
+                  <Block row marginVertical={sizes.m}>
+                    <Button
+                      shadow={false}
+                      radius={sizes.m}
+                      marginHorizontal={sizes.sm}
+                      color="rgba(255,255,255,0.2)"
+                      outlined={String(colors.white)}
+                      onPress={() => handleSocialLink('twitter')}>
+                      <Ionicons
+                        size={18}
+                        name="logo-instagram"
+                        color={colors.white}
+                      />
+                    </Button>
+                    <Button
+                      shadow={false}
+                      radius={sizes.m}
+                      color="rgba(255,255,255,0.2)"
+                      outlined={String(colors.white)}
+                      onPress={() => handleSocialLink('dribbble')}>
+                      <Ionicons
+                        size={18}
+                        name="globe-outline"
+                        color={colors.white}
+                      />
+                    </Button>
+                      <Button
+                      shadow={false}
+                      radius={sizes.m}
+                      marginHorizontal={sizes.sm}
+                      color="rgba(255,255,255,0.2)"
+                      outlined={String(colors.white)}
+                      onPress={() => handleSocialLink('twitter')}>
+                      <Ionicons
+                        size={18}
+                        name="logo-facebook"
+                        color={colors.white}
+                      />
+                    </Button>
                   </Block>
-                </Button>
-                <Button
-                  shadow={false}
-                  radius={sizes.m}
-                  marginHorizontal={sizes.sm}
-                  color="rgba(255,255,255,0.2)"
-                  outlined={String(colors.white)}
-                  onPress={() => handleSocialLink('twitter')}>
-                  <Ionicons
-                    size={18}
-                    name="logo-twitter"
-                    color={colors.white}
-                  />
-                </Button>
-                <Button
-                  shadow={false}
-                  radius={sizes.m}
-                  color="rgba(255,255,255,0.2)"
-                  outlined={String(colors.white)}
-                  onPress={() => handleSocialLink('dribbble')}>
-                  <Ionicons
-                    size={18}
-                    name="logo-dribbble"
-                    color={colors.white}
-                  />
-                </Button>
-              </Block>
             </Block>
+              </Block>
           </Image>
 
           {/* profile: stats */}
@@ -134,7 +131,7 @@ const Profile = ({navigation, route}) => {
             flex={0}
             radius={sizes.sm}
             shadow={!isAndroid} // disabled shadow on Android due to blur overlay + elevation issue
-            marginTop={-sizes.l}
+            marginTop={-sizes.m}
             marginHorizontal="8%"
             color="rgba(255,255,255,0.2)">
             <Block
@@ -149,16 +146,7 @@ const Profile = ({navigation, route}) => {
               paddingVertical={sizes.sm}
               renderToHardwareTextureAndroid>
               <Block align="center">
-                <Text h5>{user?.stats?.posts}</Text>
-                <Text>{t('profile.posts')}</Text>
-              </Block>
-              <Block align="center">
-                <Text h5>{(user?.stats?.followers || 0) / 1000}k</Text>
-                <Text>{t('profile.followers')}</Text>
-              </Block>
-              <Block align="center">
-                <Text h5>{(user?.stats?.following || 0) / 1000}k</Text>
-                <Text>{t('profile.following')}</Text>
+                <Text>1 Small Iced Coffee.</Text>
               </Block>
             </Block>
           </Block>
@@ -172,49 +160,31 @@ const Profile = ({navigation, route}) => {
               {user?.about}
             </Text>
           </Block>
-
-          {/* profile: photo album */}
-          <Block paddingHorizontal={sizes.sm} marginTop={sizes.s}>
-            <Block row align="center" justify="space-between">
-              <Text h5 semibold>
-                {t('common.album')}
-              </Text>
-              <Button>
-                <Text p primary semibold>
-                  {t('common.viewall')}
+            {/*Activate Button*/}
+            <Block padding={sizes.padding}>
+                <Button flex={1} gradient={gradients.primary} marginBottom={sizes.base} shadow>
+                  <Text white bold transform="uppercase">
+                    Activate Coupon
+                  </Text>
+                </Button>
+                <Text center size={10}>
+                    Don't activate the coupon until you're ready to use.
+                    Couponse are only redeemable for 15 minutes once activated.
                 </Text>
-              </Button>
             </Block>
-            <Block row justify="space-between" wrap="wrap">
-              <Image
-                resizeMode="cover"
-                source={assets?.photo1}
-                style={{
-                  width: IMAGE_VERTICAL_SIZE + IMAGE_MARGIN / 2,
-                  height: IMAGE_VERTICAL_SIZE * 2 + IMAGE_VERTICAL_MARGIN,
-                }}
-              />
-              <Block marginLeft={sizes.m}>
-                <Image
-                  resizeMode="cover"
-                  source={assets?.photo2}
-                  marginBottom={IMAGE_VERTICAL_MARGIN}
-                  style={{
-                    height: IMAGE_VERTICAL_SIZE,
-                    width: IMAGE_VERTICAL_SIZE,
-                  }}
-                />
-                <Image
-                  resizeMode="cover"
-                  source={assets?.photo3}
-                  style={{
-                    height: IMAGE_VERTICAL_SIZE,
-                    width: IMAGE_VERTICAL_SIZE,
-                  }}
-                />
-              </Block>
+            {/*Mock Coupon*/}
+            <Block padding={sizes.l}>
+                <Block card padding={0} marginTop={sizes.sm}>
+                    <Image
+                      background
+                      resizeMode="cover"
+                      source={assets.background}
+                      radius={sizes.cardRadius}>
+                        <Block color="rgba(0,0,0,0.1)" padding={sizes.l} height={400}>
+                        </Block>
+                    </Image>
+                </Block>
             </Block>
-          </Block>
         </Block>
       </Block>
     </Block>
