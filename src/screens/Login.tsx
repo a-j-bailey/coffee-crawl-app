133810ef8163a8fb6ddc94f9159d1407735e2620
Adam Bailey <ajbailey564@gmail.com>
2023-05-16 22:39:24 -0500

    add forgot password ui

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -116,9 +116,71 @@ const Login = () => {
                                 </Text>
                             </Button>
                         </Block>
+                        
                     </Block>
+                    <Button
+                        onPress={() => setView('Forgot Password')}>
+                        <Text white bold>
+                            Forgot Password
+                        </Text>
+                    </Button>
                 </Block>
         </Image>
+        )
+    } else if (view == 'Forgot Password') {
+        return (
+            <Image
+                background
+                source={{uri: imageLink}}
+                paddingHorizontal={sizes.padding}
+                style={{flex: 1}}>
+                <Block safe>
+                    <Block
+                        blur
+                        intensity={90}
+                        flex={0}
+                        radius={sizes.sm}
+                        overflow="hidden"
+                        tint={colors.blurTint}
+                        padding={sizes.padding}
+                        marginVertical={sizes.l}
+                        marginHorizontal={sizes.padding}>
+                        <Block paddingHorizontal={sizes.sm} flex={0}>
+                            <Text h4 center color={colors.primary} margin={sizes.md}>
+                                Reset Password
+                            </Text>
+                            <Input
+                                autoCapitalize="none"
+                                marginVertical={sizes.m}
+                                label={t('common.email')}
+                                keyboardType="email-address"
+                                placeholder="email@example.com"
+                                success={null}
+                                danger={null}
+                                onChangeText={(text) => setEmail(text)}
+                                value={email}
+                                color={colors.dark}
+                            />
+                            <Button
+                                gradient={gradients.dark}
+                                shadow={!isAndroid}
+                                marginTop={sizes.m}
+                                marginBottom={sizes.s}
+                                onPress={() => resetPassword()}>
+                                <Text bold white transform="uppercase">
+                                    Reset Password
+                                </Text>
+                            </Button>
+                        </Block>
+                    </Block>
+                    <Button
+                        onPress={() => setView('Login')}>
+                        <Text white bold>
+                            Nevermind, I remember it now.
+                        </Text>
+                    </Button>
+                </Block>
+            </Image>
         )
     } else {
         return (
