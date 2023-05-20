83fe49ad91952a92af0177210bcad3eedd41d1c7
Adam Bailey <adambailey@Chelises-MacBook-Pro.local>
2023-05-20 17:15:05 -0500

    paywall wording

--- src/screens/Pro.tsx
+++ src/screens/Pro.tsx
@@ -29,38 +29,31 @@ const Pro = () => {
                         Coffee Crawl 2023
                     </Text>
 
-                    <Text marginBottom={sizes.padding}>
-                        The 2023 Tulsa Coffee Crawl was specifically designed to give you
-                        the opportunity to explore new coffee shops and cafes across town.
+                    <Text marginBottom={sizes.padding} center>
+                        The 2023 Tulsa Coffee Crawl is specifically designed to give you
+                        the opportunity to explore new coffee shops and cafes across Tulsa.
                     </Text>
 
-                    <Text semibold>- 20+ Different Cafes</Text>
-                    <Text semibold>- 20+ Cups of Coffee</Text>
-                    <Text semibold>- 30 Days to Enjoy it All</Text>
-                    <Text semibold>- 1 Low Price</Text>
+                    <Text center semibold>20+ Different Cafes</Text>
+                    <Text center semibold>20+ Cups of Coffee</Text>
+                    <Text center semibold>31 Days to Enjoy it All</Text>
 
-                    <Text marginVertical={sizes.padding}>
-                        Once you've purchased you're ticket you'll be all set to partake in some amazing coffee.
-                        You'll receive a coupon for one free hot or iced coffee from each cafe.
+                    <Text marginVertical={sizes.padding} center>
+                        Soon you'll be all set to partake in some amazing coffee.
+                        Each participating cafe is offering a coupon for a minimum of a free small iced coffee!
                     </Text>
                     
-                    <Text semibold>Simply present the coupon and enjoy your fresh local coffee.</Text>
+                    <Text center semibold>Simply present the coupon and enjoy your fresh local coffee.</Text>
 
-                    
                     <Button
                         gradient={gradients.primary}
-                        marginVertical={sizes.s}
-                        onPress={() =>
-                            handleWebLink(
-                                'https://www.creative-tim.com/product/soft-ui-pro-react-native',
-                            )
-                        }>
+                        marginVertical={sizes.m}>
                         <Text white bold transform="uppercase">
                             In-App Purchase Button
                         </Text>
                     </Button>
 
-                    <Button title="Learn More" padding={sizes.s} onPress={() => handleWebLink('http://coffeecrawl.club')}>
+                    <Button padding={sizes.s} onPress={() => handleWebLink('http://coffeecrawl.club')}>
                         <Text color={colors.secondary}>I still have questions. Tell me more.</Text>
                     </Button>
                 </Block>
