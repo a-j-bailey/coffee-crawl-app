e2db61c9bbb69a014c91fac09d094a0f4bf8a441
Adam Bailey <ajbailey564@gmail.com>
2023-06-14 01:21:46 -0500

    update build version

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import { vexo } from 'vexo-analytics';
 
 import App from './App';
 
-if (!__DEV__) {
+// if (!__DEV__) {
     vexo('375ade05-5f0f-4167-b717-6ecf77d0a596');
-}
+// }
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
